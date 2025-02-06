8665b2c803bd2c80a5eebd3bc4dff034fa600473
oriol-bustos <oriol.bustos2002@gmail.com>
2025-02-06 22:29:12 +0100

    feat: fnc to load all catastro info

--- nb/include_catastro_info.py
+++ nb/include_catastro_info.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sodapy import Socrata
 import requests
+import json
 
 def include_año_construccion(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -29,3 +30,26 @@ def include_año_construccion(df: pd.DataFrame) -> pd.DataFrame:
             continue
     
     return df
+
+def obtain_catastro_info_single_row(row: pd.Series) -> pd.DataFrame|None:
+    """
+    Obtains cadastral information for a single row of data.
+    Parameters:
+    row (pd.Series): A pandas Series object containing at least the 'referencia_cadastral' field.
+    Returns:
+    pd.DataFrame: A DataFrame containing the cadastral information if the request is successful.
+                  Returns None if the 'referencia_cadastral' is NaN or if the request fails.
+    """
+    ref = row['referencia_cadastral']
+    if pd.isna(ref):
+        return None
+    url = f"https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero/COVCCallejero.svc/json/Consulta_DNPRC?RefCat={ref}" 
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        datos = response.json()
+    else:
+        print(f"Error {response.status_code}: No se pudo obtener la información")
+        return None
+        
+    return datos
